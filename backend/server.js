1a17fdc54e7a6e78cf2eab15d57eb606ac3aa9ec
Tushar Gupta <t.gupta0202@gmail.com>
2025-07-10 14:30:47 +0530

    remove upload folder

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express"); 
 const cors = require("cors");
-const path = require("path");
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
@@ -23,14 +22,6 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/resume', resumeRoutes);
-
-app.use('/uploads', express.static(path.join(__dirname, 'uploads'),
-    {
-        setHeaders: (res, path) => {
-            res.setHeader('Access-Control-Allow-Origin', 'https://resume-builder-mu-murex.vercel.app');
-        }
-    }
-));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
